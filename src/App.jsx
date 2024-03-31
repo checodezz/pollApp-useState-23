3f12668adcb2771eee1382c4dccc4b4c43efe92b
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-03-31 08:45:29 +0000

    added comments for better understanding

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 
 export default function App() {
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [formData, setFormData] = useState(false);
+
+  const handleChangeEvent = (event, questionId) => {
+    const value = event.target.value;
+    setSelectedOptions((prevValue) => {
+      //here it is filtering the objects which doesn't matches with the questionId.
+      // Remove any previous selection for the current questionId.
+      const updatedObj = prevValue.filter((option) => option.id !== questionId);
+      // Add the new selection, with the previous values, ensuring only one selection per question.
+      return [...updatedObj, { id: questionId, value: value }];
+    });
+    console.log(selectedOptions);
+  };
+
+  const handleSubmitEvent = () => {
+    if (selectedOptions.length >= questions.length) {
+      setFormData(true);
+    }
+  };
+
   const questions = [
     {
       id: 1,
@@ -21,16 +43,35 @@ export default function App() {
   return (
     <main>
       <h1>Polling App</h1>
-      <ul>
-        {questions.map((question) => (
-          <div>
-            <h2>{question.question}</h2>
-            <li>
-              {question.options.map((opt) => <input type="radio"/>{opt})}
-            </li>
-          </div>
-        ))}
-      </ul>
+     {!formData && <div>
+        <form onSubmit={handleSubmitEvent}>
+          <ul>
+            {questions.map((question) => (
+              <div key={question.id}>
+                <h2>{question.question}</h2>
+
+                {question.options.map((option, index) => (
+                  <li key={index}>
+                    <label>
+                      <input
+                        type="radio"
+                        onChange={(event) =>
+                          handleChangeEvent(event, question.id)
+                        }
+                        name={`question_${question.id}`}
+                        value={option}
+                      />
+                      {option}
+                    </label>
+                  </li>
+                ))}
+              </div>
+            ))}
+          </ul>
+          <button type="submit">Submit</button>
+        </form>
+      </div>}
+      {formData && <h2>Thank You for Participating!</h2>}
     </main>
   );
 }
